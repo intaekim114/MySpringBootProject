5e8f807d053d38cebe72aa8f25bbc78c15bf943d
Intae Kim <121932300+intaekim114@users.noreply.github.com>
2025-05-15 13:41:06 +0900

    WebApp update

--- src/main/java/com/basic/myspringbootapp/MySpringBootAppApplication.java
+++ src/main/java/com/basic/myspringbootapp/MySpringBootAppApplication.java
@@ -1,13 +1,17 @@
 package com.basic.myspringbootapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MySpringBootAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MySpringBootAppApplication.class, args);
+//		SpringApplication.run(MySpringBootAppApplication.class, args);
+		SpringApplication application = new SpringApplication(MySpringBootAppApplication.class);
+		application.setWebApplicationType (WebApplicationType.NONE );
+		application.run (args);
 	}
 
 }
