f1c305d3813fd30cf51dac45fbd685a10b011c73
Intae Kim <121932300+intaekim114@users.noreply.github.com>
2025-05-15 20:27:41 +0900

    banner update

--- src/main/java/com/basic/myspringbootapp/MySpringBootAppApplication.java
+++ src/main/java/com/basic/myspringbootapp/MySpringBootAppApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // 굉장히 많은 일을 하고 있음
 public class MySpringBootAppApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(MySpringBootAppApplication.class, args);
-		SpringApplication application = new SpringApplication(MySpringBootAppApplication.class);
-		application.setWebApplicationType (WebApplicationType.NONE );
+		//SpringApplication.run(MySpringBootAppApplication.class, args);
+		SpringApplication application = new SpringApplication(MySpringBootAppApplication.class); // 객체 직접 생성
+		// 기본적으로 WebApplicationType은 웹 어플리케이션이다.
+		application.setWebApplicationType (WebApplicationType.SERVLET ); // 웹 앱의 기본적인 타입은 SERVLET이다.
+
 		application.run (args);
 	}
 
