dcfd313644e1bf8bc941c5b8326a21a717b2a936
Intae Kim <121932300+intaekim114@users.noreply.github.com>
2025-05-19 14:52:46 +0900

    ㅆTestConfig add

--- src/main/java/com/basic/myspringboot/config/TestConfig.java
+++ src/main/java/com/basic/myspringboot/config/TestConfig.java
@@ -7,7 +7,9 @@ import org.springframework.context.annotation.Configuration;
 public class TestConfig {
     @Bean
     public CustomerVO customerVO() {
-        return CustomerVO.builder()
+        return CustomerVO.builder() // 내부적으로 CustomerVOBuilder 가 생성
+                .mode("테스트 환경")
+                .rate(0.5)
                 .build();
     }
 }
