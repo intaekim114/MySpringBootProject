10d2e3cd45ed0287b28d72fd9fdd7f0ab944a0bf
Intae Kim <121932300+intaekim114@users.noreply.github.com>
2025-05-19 14:50:23 +0900

    CustomerVO add

---
+++ src/main/java/com/basic/myspringboot/config/TestConfig.java
@@ -0,0 +1,13 @@
+package com.basic.myspringboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestConfig {
+    @Bean
+    public CustomerVO customerVO() {
+        return CustomerVO.builder()
+                .build();
+    }
+}
