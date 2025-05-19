5f947db216d6ce981eb733f6d82ba6fa47b10982
Intae Kim <121932300+intaekim114@users.noreply.github.com>
2025-05-19 14:54:25 +0900

    ãProdConfig add

---
+++ src/main/java/com/basic/myspringboot/config/ProdConfig.java
@@ -0,0 +1,15 @@
+package com.basic.myspringboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProdConfig {
+    @Bean
+    public CustomerVO customerVO() {
+        return CustomerVO.builder() // 내부적으로 CustomerVOBuilder 가 생성
+                .mode("운영 환경")
+                .rate(1.5)
+                .build();
+    }
+}
