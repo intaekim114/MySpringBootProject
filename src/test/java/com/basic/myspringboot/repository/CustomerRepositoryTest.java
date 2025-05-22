7380fb59f8fa7456d3dfa8ea7d89a545e99e3084
Intae Kim <121932300+intaekim114@users.noreply.github.com>
2025-05-22 15:37:11 +0900

    CustomerRepositoryTest add

---
+++ src/test/java/com/basic/myspringboot/repository/CustomerRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.basic.myspringboot.repository;
+
+import com.basic.myspringboot.entity.Customer;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional // DB에 입력한 결과가 남지 않고 롤백된다.
+//@DataJpaTest
+class CustomerRepositoryTest {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void testFindBy() {
+        Optional<Customer> optionalCustomer = customerRepository.findById(1L);
+        assertThat(optionalCustomer).isNotEmpty();
+
+    }
+
+    @Test
+    @Rollback(value = false) //Rollback 처리 하지 마세요
+    @Disabled
+    void testCreateCustomer() {
+        //Given (준비 단계)
+        Customer customer = new Customer();
+        customer.setCustomerId("A002");
+        customer.setCustomerName("스프링2");
+        //When (실행 단계)
+        Customer addCustomer = customerRepository.save(customer);
+        //Then (검증 단계)
+        assertThat(addCustomer).isNotNull();
+        assertThat(addCustomer.getCustomerName()).isEqualTo("스프링2");
+    }
+
+
+}
