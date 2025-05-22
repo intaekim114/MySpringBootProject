d33391c526f24ea43be7e6506cf1713295a2b32c
Intae Kim <121932300+intaekim114@users.noreply.github.com>
2025-05-22 13:22:04 +0900

    Query Method add

--- src/main/java/com/basic/myspringboot/repository/CustomerRepository.java
+++ src/main/java/com/basic/myspringboot/repository/CustomerRepository.java
@@ -3,5 +3,13 @@ package com.basic.myspringboot.repository;
 import com.basic.myspringboot.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    //Query Method 정의하면 JPA가 JPQL(java persistence query language)
+    Optional<Customer> findByCustomerId(String id); // 메소드 명이 중요하다. findbyCustomerId 라고 하면 오류 발생
+    List<Customer> findByCutomerName(String name);
+
+
 }
