dfeb4d00865fff182f03b7ebdce6b0bfa874b616
Intae Kim <121932300+intaekim114@users.noreply.github.com>
2025-05-22 10:43:11 +0900

    hibernate sql add

--- src/main/java/com/basic/myspringboot/entity/Customer.java
+++ src/main/java/com/basic/myspringboot/entity/Customer.java
@@ -9,7 +9,7 @@ import lombok.Setter;
 @Getter @Setter
 public class Customer {
     // Primary Key, pk 값을 persistence provider가 결정해라
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     // 중복 허용하지 않고, Null 값을 허용하지 않는다.
     @Column(unique = true, nullable = false) // 중복 불가, null값 불가
@@ -17,7 +17,5 @@ public class Customer {
 
     @Column(nullable = false)
     private String customerName;
-
-
 
 }
