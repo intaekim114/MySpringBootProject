fadeb01986212cf10cc75827fdcc888fc9ae571a
Intae Kim <121932300+intaekim114@users.noreply.github.com>
2025-05-22 13:37:13 +0900

    Query Method update

--- src/main/java/com/basic/myspringboot/repository/CustomerRepository.java
+++ src/main/java/com/basic/myspringboot/repository/CustomerRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     //Query Method 정의하면 JPA가 JPQL(java persistence query language)
     Optional<Customer> findByCustomerId(String id); // 메소드 명이 중요하다. findbyCustomerId 라고 하면 오류 발생
-    List<Customer> findByCutomerName(String name);
+    List<Customer> findByCustomerNameContains(String name); // 유니크 하지 않은 값이기 때문에 여러 객체가 반환 될 수 있다
 
 
 }
